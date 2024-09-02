0918369c6eaaf3fb06240939f0582f681edb3c2b
Murvat <murvat.mustafayev@mail.ru>
2024-09-02 18:55:37 +0100

    step2

--- lesson1/index.ts
+++ lesson1/index.ts
@@ -1,28 +1,28 @@
-// let  str: string = 'str';
-// // str = 5;
+// // let  str: string = 'str';
+// // // str = 5;
 
-// const isBirthday: boolean = true;
-// let ageData: number = 4;
-// const userNameData: string = 'John';
+// // const isBirthday: boolean = true;
+// // let ageData: number = 4;
+// // const userNameData: string = 'John';
 
-// function logBrtMsg(isBirthday:boolean, userNameData:string, ageData:number):void {
-//     if (isBirthday) {
-//         console.log(`Congrats ${userNameData.toUpperCase()} with ${ageData} years`)
-//     }
+// // function logBrtMsg(isBirthday:boolean, userNameData:string, ageData:number):void {
+// //     if (isBirthday) {
+// //         console.log(`Congrats ${userNameData.toUpperCase()} with ${ageData} years`)
+// //     }
+// // }
+// const currRate = '1.05';
+// const fetchCurr = (response:string) => {
+//     const data = JSON.parse(response);
+//     return data;
 // }
-const currRate = '1.05';
-const fetchCurr = (response:string) => {
-    const data = JSON.parse(response);
-    return data;
-}
 
-function transferEurToUsd(available:boolean, amount:number, comission:number):void {
-    if (available) {
-        let res = fetchCurr(currRate) * amount *comission
-    console.log(res)
-    } else {
-        console.log('skdak')
-    }
-};
+// function transferEurToUsd(available:boolean, amount:number, comission:number):void {
+//     if (available) {
+//         let res = fetchCurr(currRate) * amount *comission
+//     console.log(res)
+//     } else {
+//         console.log('skdak')
+//     }
+// };
 
-transferEurToUsd(true,500,1.05)
+// transferEurToUsd(true,500,1.05)
