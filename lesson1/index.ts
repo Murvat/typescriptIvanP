b6ad09ae6bf7233c8d600c12f5c5231e5c670655
Murvat <murvat.mustafayev@mail.ru>
2024-09-03 18:49:37 +0100

    .

--- lesson1/index.ts
+++ lesson1/index.ts
@@ -10,19 +10,39 @@
 //         console.log(`Congrats ${userNameData.toUpperCase()} with ${ageData} years`)
 //     }
 // }
-const currRate = '1.05';
-const fetchCurr = (response:string) => {
-    const data = JSON.parse(response);
-    return data;
-}
+// const currRate = '1.05';
+// const fetchCurr = (response:string) => {
+//     const data = JSON.parse(response);
+//     return data;
+// }
 
-function transferEurToUsd(available:boolean, amount:number, comission:number):void {
-    if (available) {
-        let res = fetchCurr(currRate) * amount *comission
-    console.log(res)
-    } else {
-        console.log('skdak')
-    }
-};
+// function transferEurToUsd(available:boolean, amount:number, comission:number):void {
+//     if (available) {
+//         let res = fetchCurr(currRate) * amount *comission
+//     console.log(res)
+//     } else {
+//         console.log('skdak')
+//     }
+// };
 
-transferEurToUsd(true,500,1.05)
+// transferEurToUsd(true,500,1.05)
+
+// const obj: { age: number, username: string } = {
+//     age: 50,
+//     username:'Murvat'
+// }
+
+// function msg(data: { age: number, username: string }) {
+//     console.log(data.age, data.username);
+// }
+
+// msg(obj);
+
+// const departenst: string[] = ['dev', 'design', 'marketing'];
+// const nums: number[] = [5, 32, 8];
+// const values: number[][] = [[12], [12], [8]];
+// const report: string[] = departenst.filter((d: string) => d != 'dev')
+
+//
+
+const message: string|number='2'
